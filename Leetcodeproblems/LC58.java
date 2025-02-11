87410efe400de9031246d72d4c2ee60b10753b29
AbdealiB <abdealibhagat787@gmail.com>
2025-02-11 19:58:47 +0530

    Adding Leet Code Problem No. 58

---
+++ Leetcodeproblems/LC58.java
@@ -0,0 +1,24 @@
+package codingProblems.LeetcodeProblems;
+
+public class LC58 {
+    /*
+     * LeetCode Problem 58. Length of Last Word
+     * https://leetcode.com/problems/length-of-last-word/description/
+     */
+    public static void main(String[] args) {
+        System.out.println(lengthOfLastWord("   HellLLLo            World      ")); // Output: 5
+        System.out.println(lengthOfLastWord("")); // Output: 0
+        System.out.println(lengthOfLastWord("a ")); // Output: 1
+    }
+
+    private static int lengthOfLastWord(String a) {
+        String[] truncated = a.trim().split(" ");
+        for (int i = truncated.length - 1; i >= 0; i--) {
+            if (!(" ".equals(truncated[i]))) {
+                return truncated[i].length();
+            }
+        }
+
+        return 0;
+    }
+}
