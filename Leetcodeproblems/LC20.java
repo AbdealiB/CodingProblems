2be3291b86886c313132cae557cb59a36dcfd2af
AbdealiB <abdealibhagat787@gmail.com>
2025-02-13 10:01:36 +0530

    Adding Leet Code Problem No. 20

---
+++ Leetcodeproblems/LC20.java
@@ -0,0 +1,54 @@
+package codingProblems.LeetcodeProblems;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class LC20 {
+    /*
+     * LeetCode 20: Valid Parentheses
+     * https://leetcode.com/problems/valid-parentheses/description/
+     * Time Complexity: O(n)
+     * Space Complexity: O(n)
+     */
+
+    public static void main(String[] args) {
+        System.out.println(isValid("()")); // Output: true
+        System.out.println(isValid("()[]{}")); // Output: true
+        System.out.println(isValid("(]")); // Output: false
+        System.out.println(isValid("([)]")); // Output: false
+        System.out.println(isValid("{[]}")); // Output: true
+        System.out.println(isValid("(){}}{"));
+    }
+
+    public static boolean isValid(String s) {
+        if (s.length() == 0) {
+            return true;
+        }
+
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> map = new HashMap<>();
+        map.put('(', ')');
+        map.put('[', ']');
+        map.put('{', '}');
+
+        for (int i = 0; i < s.length(); i++) {
+            char curr = s.charAt(i);
+            if (map.containsKey(curr)) {
+                stack.push(curr);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char stacked = stack.pop();
+                char mappedValue = map.get(stacked);
+                if (!(curr == mappedValue)) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
